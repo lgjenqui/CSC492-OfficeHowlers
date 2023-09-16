a9cfc5085728e9012f6d62693ca3e6d2639bbab3
Luke Jenquin <lukemj702@gmail.com>
2023-09-16 11:16:00 -0400

    Basic communication with MariaDB Course creation established

--- HowlBack/app.js
+++ HowlBack/app.js
@@ -2,12 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const db = require("./db.js");
+
+db.sequelize.sync();
+
 app.get('/', (req, res) => {
-  console.log(req);
+  // console.log(req);
   res.send('Hello World!')
-})
+});
+
+require("./routes/course.routes")(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
 
---
+++ HowlBack/db.js
@@ -0,0 +1,22 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(process.env.MARIADB_HOST, process.env.MARIADB_USERNAME, process.env.MARIADB_PASSWORD, {
+  host: process.env.MARIADB_HOST,
+  port: process.env.MARIADB_PORT,
+  dialect: 'mariadb',
+  define: {
+    timestamps: false
+  }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.courses = require("./models/course.model.js")(sequelize, Sequelize);
+// db.instructors = require("./models/instructor.model.js")(sequelize, Sequelize);
+// db.students = require("./models/student.model.js")(sequelize, Sequelize);
+// db.teachingAssistants = require("./models/teachingassistant.model.js")(sequelize, Sequelize);
+
+module.exports = db;
