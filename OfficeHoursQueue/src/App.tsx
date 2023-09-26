8af4abd7717f6760e8b532dfa34e3b243dcedee5
cameronhimes <cameronhimes2@gmail.com>
2023-09-26 19:54:57 -0400

    Re-implemented the banner and footer image.

--- OfficeHoursQueue/src/App.tsx
+++ OfficeHoursQueue/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box } from "@mui/material";
+import * as React from "react";
+import "./App.css";
+import Banner from "./components/banner/Banner";
+import NCSULogo from "./assets/ncstate-logo.jpg";
+
+const title = "OfficeHowlers";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <React.StrictMode>
+      <Box
+        sx={{
+          width: "100%",
+          color: "white",
+          height: "150px",
+          m: 0,
+        }}
+      >
+        <Banner title="OfficeHowlers" subtitle="Think and Do"></Banner>
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: 15,
+            left: 15,
+          }}
+        >
+          <img src={NCSULogo} width="162" height="78" alt="" />
+        </Box>
+      </Box>
+    </React.StrictMode>
+  );
 }
 
-export default App
+export default App;
---
+++ OfficeHoursQueue/src/components/banner/Banner.tsx
@@ -0,0 +1,30 @@
+import Box from "@mui/material/Box";
+import { ReactNode } from "react";
+
+interface Props {
+  title: ReactNode;
+  subtitle: ReactNode;
+}
+
+const Banner = ({ title, subtitle }: Props) => {
+  return (
+    <Box sx={{ bgcolor: "#CC0000", width: "100%", height: "100%" }}>
+      <Box
+        sx={{
+          width: "fit-content",
+          position: "relative",
+          top: "50%",
+          transform: "transLateY(-50%)",
+        }}
+      >
+        <Box sx={{ fontSize: "48px", ml: "30px" }}>{title}</Box>
+
+        <Box sx={{ margin: "auto", width: "fit-content", fontSize: "32px" }}>
+          {subtitle}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Banner;
