4933d40b041954d88c910ad42f66983e0ea0a615
cameronhimes <cameronhimes2@gmail.com>
2023-09-26 17:17:49 -0400

    Regenerated the frontend using Vite (as opposed to CRA)

--- OfficeHoursQueue/src/App.tsx
+++ OfficeHoursQueue/src/App.tsx
@@ -1,54 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  // Make a call to the backend endpoint - for testing connectivity 
-  testCall()
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <div className="Banner">
-        <div className="Title">
-          <b style={{color:'white', fontSize:30}}>Office Howlers</b>
-          <p style={{color:'white', fontSize:20, marginTop:0}}>Think and Do</p>
-          <p>{data}</p>
-        </div>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <div className="Body">
-        <div className="Login" style={{textAlign:"center"}}>
-          <b style={{color:'white', fontSize:20, textAlign:"center"}}>Local Sign On</b>
-          <div style={{textAlign:"left"}}>
-            <p style={{color:'white', fontSize:13, marginBottom:3}}>Username</p>
-            <input style={{marginTop:0, borderRadius:"5px", width: "97%"}}></input>
-            <p style={{color:'white', fontSize:13, marginBottom:3}}>Password</p>
-            <input style={{marginTop:0, borderRadius:"5px", width: "97%"}}></input>
-          </div>
-          <button className="Login-button">Login</button>
-          <br></br>
-          <a href = "" style={{color:'white', fontSize:13, marginBottom:3}}>Forgot Password</a>
-          <hr></hr>
-          <b style={{color:'white', fontSize:20}}>SSO</b>
-          <br></br>
-          <button className="SSO-button">
-            <img src="NCSU Belltower Image.png" style={{float:"left"}}></img>
-              <p style={{textAlign:"center", marginBottom:0}}>Campus ID Login</p><p style={{fontSize:12, marginTop:0}}>Students/Faculty/Staff</p>
-      </button>
-            
-        </div>
-        <b style={{position:"absolute", bottom:30, left: 15}}>NC STATE</b>
-        <br></br>
-        <p style={{position:"absolute", bottom:0, left: 15}}>UNIVERSITY</p>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
       </div>
-    </div>
-  );
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-async function testCall() {
-  const response = await fetch("http://localhost:8080/test");
-  console.log(response.json())
-}
-
-export default App;
+export default App
