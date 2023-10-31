696c94d76bb3a537ed1b3f731976b1cbaa2ee29f
Luke Jenquin <lukemj702@gmail.com>
2023-10-30 23:10:01 -0400

    Added validation that user is elevated privilege user to start session

--- HowlBack/controllers/session.controller.ts
+++ HowlBack/controllers/session.controller.ts
@@ -3,7 +3,7 @@ import Course from '../models/course.model';
 import User from '../models/user.model';
 import Session from '../models/session.model';
 import { retrieveUser, findOrCreateUser } from '../services/user.service';
-import { isValidInstructorForCourse, isValidUserForCourse } from '../services/course.service';
+import { isValidInstructorForCourse, isValidInstructorOrAssistantForCourse, isValidUserForCourse } from '../services/course.service';
 import { UUID } from 'crypto';
 
 export const createSession = async (req: Request, res: Response): Promise<void> => {
@@ -11,11 +11,14 @@ export const createSession = async (req: Request, res: Response): Promise<void> 
     // Create a course in the database
     const createdSession = await Session.create();
     const instructorUser = await retrieveUser(req.headers['x-shib_mail'] as string);
-    if(instructorUser) {
+    if(instructorUser ) {
       await createdSession.setUser(instructorUser);
       await Promise.all(req.body.courseIDs.map(async (id: UUID) => {
         const course = await Course.findByPk(id);
-        return createdSession.addCourse(course);
+        if ( await isValidInstructorOrAssistantForCourse(instructorUser.email, course)) {
+          return createdSession.addCourse(course);
+        }
+        return Promise.reject("User not eligible to start Session for Course: " + course.name);
       }));
       res.status(201).json(createdSession);
     }
