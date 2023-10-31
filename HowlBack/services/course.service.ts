a6e75a0ecb1aeedcdc83790e7a87432f0abb055e
Luke Jenquin <lukemj702@gmail.com>
2023-10-30 23:10:43 -0400

    Added function previous commit relies on hehe

--- HowlBack/services/course.service.ts
+++ HowlBack/services/course.service.ts
@@ -5,6 +5,13 @@ export async function isValidInstructorForCourse( email: string, course: Course 
     return instructors.find((user) => { return user.email == email });
 }
 
+export async function isValidInstructorOrAssistantForCourse( email: string, course: Course ) {
+    const assistants = await course.getAssistants();
+    const instructors = await course.getInstructors();
+    const allElevatedUsers = assistants.concat(instructors);
+    return allElevatedUsers.find((user) => { return user.email == email });
+}
+
 export async function isValidUserForCourse( email: string, course: Course ) {
     const instructors = await course.getInstructors();
     const assistants = await course.getAssistants();
