b6c604873db6ba6a951d0ec87480aecf2e9c6c08
Cam Himes <cshimes@ncsu.edu>
2023-10-31 13:42:02 -0400

    Changed the backend methods to grab the User instead of the username

--- HowlBack/controllers/user.controller.ts
+++ HowlBack/controllers/user.controller.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from 'express';
-import User from '../models/user.model';
-import {retrieveUser, findOrCreateUser} from '../services/user.service';
+import {retrieveUser} from '../services/user.service';
 
-export const getUsername = async (req: Request, res: Response): Promise<void> => {
+export const getUser = async (req: Request, res: Response): Promise<void> => {
   try {
-    const firstname = req.headers['x-shib_givenname'];
-    res.status(200).json(firstname);
+    const user = retrieveUser(req.headers['x-shib_mail'] as string);
+    res.status(200).json(user);
   }
   catch(error) {
-    res.status(500).json({ message: 'Error retrieving username', error: error.message });
+    res.status(500).json({ message: 'Error retrieving user', error: error.message });
 
   }
 };
--- HowlBack/routes/user.routes.ts
+++ HowlBack/routes/user.routes.ts
@@ -1,9 +1,9 @@
-import express, { Application, Request, Response, Router } from 'express';
-import { getUsername } from '../controllers/user.controller';
+import express, { Router } from 'express';
+import { getUser } from '../controllers/user.controller';
 import cors from "cors";
 
 const userRouter: Router = express.Router();
-userRouter.get('/', cors(), getUsername );
+userRouter.get('/', cors(), getUser );
 
 export default userRouter;
 
