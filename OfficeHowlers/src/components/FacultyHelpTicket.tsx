54140c6f003460b2f956c59df3ea5e5a896bdec5
Cam Himes <cshimes@ncsu.edu>
2023-12-02 17:25:46 -0500

    Style changes to the help session queue page w/ Bagya's help

--- OfficeHowlers/src/components/FacultyHelpTicket.tsx
+++ OfficeHowlers/src/components/FacultyHelpTicket.tsx
@@ -38,183 +38,202 @@ const FacultyHelpTicket = ({ ticket }: Props) => {
       <Card
         sx={{
           mt: "20px",
-          backgroundColor: "#CC0000",
+          // backgroundColor: "#CC0000",
+          // background: "#EDEDED",
+          border: "2px solid #CC0000",
           backgroundClip: "padding-box",
-          color: "white",
+          color: "black",
           borderRadius: "15px",
           ":hover": {
             cursor: "default",
           },
+          padding: 0,
         }}
       >
-        <CardContent>
-          <Typography sx={{ fontSize: 42, mb: "0", textAlign: "center" }}>
-            {ticket.User.firstName} {ticket.User.lastName}
-          </Typography>
-          <Box sx={{ textAlign: "center", mt: "5px" }}>
-            <Typography
-              sx={{ display: "inline", fontSize: 23, mb: "5px", mr: "45px" }}
-            >
-              <b>Course: </b>
-              {ticket.Course.name}
-            </Typography>
-            <Typography
-              sx={{ display: "inline", fontSize: 23, mb: "5px", mr: "45px" }}
-            >
-              <b>Location: </b>Virtual
-            </Typography>
-            {!ticket.active && (
-              <Typography
-                sx={{ display: "inline", fontSize: 23, mb: "5px", mr: "45px" }}
-              >
-                <b>Waiting for: </b>
-                {getTimeDiffStr(ticket.createdAt, dayjs())}
-              </Typography>
-            )}
-            {ticket.active && (
-              <Typography
-                sx={{ display: "inline", fontSize: 23, mb: "5px", mr: "45px" }}
-              >
-                <b>Being Helped For: </b>
-                {getTimeDiffStr(dayjs(localStorage.getItem("time")), dayjs())}
-              </Typography>
-            )}
-            <Divider
-              sx={{
-                m: "auto",
-                borderTop: "1px solid white",
-                width: "95%",
-                mt: "10px",
-                mb: "10px",
-              }}
-            />
-          </Box>
-          <Box sx={{ m: "auto", mt: "10px" }}>
-            <Typography sx={{ fontSize: 23, mb: "5px", fontWeight: "bold" }}>
-              Assignment:
-            </Typography>
-            <Typography
-              sx={{
-                // background: "#D9D9D9",
-                color: "white",
-                width: "90%",
-                paddingTop: "3px",
-                paddingBottom: "3px",
-                paddingLeft: "15px",
-                fontSize: 23,
-                mb: "15px",
-                ml: "20px",
-                mt: "5px",
-                border: "1px solid white",
-              }}
-            >
-              - {ticket.assignment}
-            </Typography>
-            <Typography sx={{ fontSize: 23, mb: "5px", fontWeight: "bold" }}>
-              Problem description:
-            </Typography>
-            <Typography
-              sx={{
-                // background: "#D9D9D9",
-                color: "white",
-                width: "90%",
-                paddingTop: "3px",
-                paddingBottom: "3px",
-                paddingLeft: "15px",
-                fontSize: 23,
-                mb: "15px",
-                ml: "20px",
-                mt: "5px",
-                border: "1px solid white",
-              }}
-            >
-              - {ticket.problemDescription}
-            </Typography>
-            <Typography
-              sx={{
-                fontSize: 23,
-                fontWeight: "bold",
-              }}
-            >
-              What they've tried:
-            </Typography>
-            <Typography
-              sx={{
-                // background: "#D9D9D9",
-                color: "white",
-                width: "90%",
-                paddingTop: "3px",
-                paddingBottom: "3px",
-                paddingLeft: "15px",
-                fontSize: 23,
-                mb: "5px",
-                ml: "20px",
-                mt: "5px",
-                border: "1px solid white",
-              }}
-            >
-              - {ticket.solutionAttempt}
-            </Typography>
-          </Box>
+        <CardContent sx={{ padding: 0 }}>
           <Box
             sx={{
-              textAlign: "center",
-              mt: "35px",
+              background: "#CC0000",
+              color: "white",
+              pt: "10px",
+
+              pb: "15px",
             }}
           >
-            {!ticket.active && (
-              <Button
+            <Typography sx={{ fontSize: 42, mb: "0", textAlign: "center" }}>
+              {ticket.User.firstName} {ticket.User.lastName}
+            </Typography>
+            <Box sx={{ textAlign: "center", mt: "5px" }}>
+              <Typography
+                sx={{ display: "inline", fontSize: 23, mb: "5px", mr: "45px" }}
+              >
+                <b>Course: </b>
+                {ticket.Course.name}
+              </Typography>
+              <Typography
+                sx={{ display: "inline", fontSize: 23, mb: "5px", mr: "45px" }}
+              >
+                <b>Location: </b>Virtual
+              </Typography>
+              {!ticket.active && (
+                <Typography
+                  sx={{
+                    display: "inline",
+                    fontSize: 23,
+                    mb: "5px",
+                    mr: "45px",
+                  }}
+                >
+                  <b>Waiting for: </b>
+                  {getTimeDiffStr(ticket.createdAt, dayjs())}
+                </Typography>
+              )}
+              {ticket.active && (
+                <Typography
+                  sx={{
+                    display: "inline",
+                    fontSize: 23,
+                    mb: "5px",
+                    mr: "45px",
+                  }}
+                >
+                  <b>Being Helped For: </b>
+                  {getTimeDiffStr(dayjs(localStorage.getItem("time")), dayjs())}
+                </Typography>
+              )}
+            </Box>
+          </Box>
+          <Box sx={{ m: "15px" }}>
+            <Box sx={{ m: "auto", mt: "10px" }}>
+              <Typography sx={{ fontSize: 23, mb: "5px", fontWeight: "bold" }}>
+                Assignment:
+              </Typography>
+              <Typography
                 sx={{
-                  fontSize: 20,
-                  color: "#CC0000",
-                  backgroundColor: "white",
-                  ":hover": {
-                    backgroundColor: "#D9D9D9",
-                  },
-                  mr: "20px",
-                }}
-                variant="contained"
-                onClick={() => {
-                  setStudentTicketStatusHandler(true);
+                  background: "white",
+                  color: "black",
+                  width: "90%",
+                  paddingTop: "3px",
+                  paddingBottom: "3px",
+                  paddingLeft: "15px",
+                  fontSize: 23,
+                  mb: "15px",
+                  ml: "20px",
+                  mt: "5px",
+                  border: "1px solid black",
+                  borderRadius: "10px",
                 }}
               >
-                Help student
-              </Button>
-            )}
-            {!ticket.active && (
-              <Button
+                {ticket.assignment}
+              </Typography>
+              <Typography sx={{ fontSize: 23, mb: "5px", fontWeight: "bold" }}>
+                Problem description:
+              </Typography>
+              <Typography
                 sx={{
-                  fontSize: 20,
-                  color: "#CC0000",
-                  backgroundColor: "white",
-                  ":hover": {
-                    backgroundColor: "#D9D9D9",
-                  },
-                }}
-                variant="contained"
-                onClick={() => {}}
-              >
-                Remove from queue
-              </Button>
-            )}
-            {ticket.active && (
-              <Button
-                sx={{
-                  fontSize: 20,
-                  color: "#CC0000",
-                  backgroundColor: "white",
-                  ":hover": {
-                    backgroundColor: "#D9D9D9",
-                  },
-                }}
-                variant="contained"
-                onClick={() => {
-                  setStudentTicketStatusHandler(false);
+                  background: "white",
+                  color: "black",
+                  width: "90%",
+                  paddingTop: "3px",
+                  paddingBottom: "3px",
+                  paddingLeft: "15px",
+                  fontSize: 23,
+                  mb: "15px",
+                  ml: "20px",
+                  mt: "5px",
+                  border: "1px solid black",
+                  borderRadius: "10px",
                 }}
               >
-                Return to queue
-              </Button>
-            )}
+                {ticket.problemDescription}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: 23,
+                  fontWeight: "bold",
+                }}
+              >
+                What they've tried:
+              </Typography>
+              <Typography
+                sx={{
+                  background: "white",
+                  color: "black",
+                  width: "90%",
+                  paddingTop: "3px",
+                  paddingBottom: "3px",
+                  paddingLeft: "15px",
+                  fontSize: 23,
+                  mb: "5px",
+                  ml: "20px",
+                  mt: "5px",
+                  border: "1px solid black",
+                  borderRadius: "10px",
+                }}
+              >
+                {ticket.solutionAttempt}
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                textAlign: "center",
+                mt: "35px",
+              }}
+            >
+              {!ticket.active && (
+                <Button
+                  sx={{
+                    fontSize: 20,
+                    color: "white",
+                    backgroundColor: "#CC0000",
+                    ":hover": {
+                      backgroundColor: "#D9D9D9",
+                    },
+                    mr: "20px",
+                  }}
+                  variant="contained"
+                  onClick={() => {
+                    setStudentTicketStatusHandler(true);
+                  }}
+                >
+                  Help student
+                </Button>
+              )}
+              {!ticket.active && (
+                <Button
+                  sx={{
+                    fontSize: 20,
+                    color: "white",
+                    backgroundColor: "#CC0000",
+                    ":hover": {
+                      backgroundColor: "#D9D9D9",
+                    },
+                  }}
+                  variant="contained"
+                  onClick={() => {}}
+                >
+                  Remove from queue
+                </Button>
+              )}
+              {ticket.active && (
+                <Button
+                  sx={{
+                    fontSize: 20,
+                    color: "#CC0000",
+                    backgroundColor: "white",
+                    ":hover": {
+                      backgroundColor: "#D9D9D9",
+                    },
+                  }}
+                  variant="contained"
+                  onClick={() => {
+                    setStudentTicketStatusHandler(false);
+                  }}
+                >
+                  Return to queue
+                </Button>
+              )}
+            </Box>
           </Box>
         </CardContent>
       </Card>
